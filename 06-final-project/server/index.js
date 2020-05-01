e4df97e5a1bedb88d7f74d23ce0e1ff28e2670b4
Caleb Chang <changc42@gmail.com>
2020-05-01 11:31:40 -0400

    added styling and foundation for landing page

--- 06-final-project/server/index.js
+++ 06-final-project/server/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
+const path = require("path");
 const endpoints = require("./auth/endpoints");
 const { client_id, state } = require("./auth/keys");
 const scopes = require("./auth/scopes");
@@ -8,12 +9,11 @@ const scopes = require("./auth/scopes");
 let server = http.createServer();
 
 server.on("request", (req, res) => {
-  if (req.url == "/") {
+  if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/html" });
     let htmlFile = fs.createReadStream("./html/landing.html");
     htmlFile.pipe(res);
-  }
-  if (req.url == "/auth") {
+  } else if (req.url == "/auth") {
     let query = {
       client_id: client_id,
       redirect_uri: "http://localhost:3000/authredirect",
@@ -25,17 +25,24 @@ server.on("request", (req, res) => {
     console.log(queryString);
     res.writeHead(302, { Location: `${endpoints.oauth}?${queryString}` });
     res.end();
-  }
-  if (req.url.startsWith("/authredirect")) {
+  } else if (req.url.startsWith("/authredirect")) {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.write(
       "You have been successfully redirected from google's auth page back to the app"
     );
     res.end();
-  }
-  if (req.url.startsWith("/testForm")) {
+  } else if (req.url.startsWith("/testForm")) {
     res.write("made it");
     res.end();
+  } else {
+    let target = path.join(__dirname, "static", req.url);
+    if (fs.existsSync(target)) {
+      let staticFile = fs.createReadStream(target);
+      staticFile.pipe(res);
+    } else {
+      res.write("404");
+      res.end();
+    }
   }
 });
 server.listen(3000, () => {
