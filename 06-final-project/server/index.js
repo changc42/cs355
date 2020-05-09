18038453bd6890b82b59d94a01a29555f91513d1
Caleb Chang <changc42@gmail.com>
2020-05-09 17:58:47 -0400

    handled nlp api exception

--- 06-final-project/server/index.js
+++ 06-final-project/server/index.js
@@ -138,11 +138,19 @@ server.on("request", (req, res) => {
     });
   } else if (req.url === "/results") {
     let sum = 0;
+    let total = myMessageList.length;
     myMessageList.forEach((e) => {
-      sum += e.sentAnalysis.documentSentiment.score;
+      console.log(moreReducedMsg(e));
+      if (e.sentAnalysis.error) {
+        total--;
+      } else sum += e.sentAnalysis.documentSentiment.score;
     });
-    let avg = sum / myMessageList.length;
+    let avg = sum / total;
     let body = myMessageList.map((e) => {
+      let sentScore;
+      if (e.sentAnalysis.error) {
+        sentScore = "message not supported for analysis";
+      } else sentScore = e.sentAnalysis.documentSentiment.score;
       return `<br><div><p>From: ${
         e.headers.filter((e) => e.name === "From")[0].value
       }</p><p>To: ${
@@ -151,9 +159,9 @@ server.on("request", (req, res) => {
         e.headers.filter((e) => e.name === "Date")[0].value
       }</p><p>Subject: ${
         e.headers.filter((e) => e.name === "Subject")[0].value
-      }</p><p>sentiment score: ${
-        e.sentAnalysis.documentSentiment.score
-      }</p><p>Content: ${e.content}</p></div><br>`;
+      }</p><p>sentiment score: ${sentScore}</p><p>Content: ${
+        e.content
+      }</p></div><br>`;
     });
     body = body.join("");
     body = `<div><h1>average score: ${avg}</h1><div>` + body;
